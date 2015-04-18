ba417ec8baba7bcb3e0e26191ee3bef29885f91b
brenoqueiroz <brenoqueiroz@gmail.com>
2015-04-18 17:17:35 -0300

    arquivos deixados pra traz =)

--- www/js/service-github.js
+++ www/js/service-github.js
@@ -14,31 +14,31 @@ angular.module('devmeetup.github')
 
 .factory('foto.service', ['$http', function($http) {
 	var enviarEmail = function(email, imageURI) {
-        if(window.plugins && window.plugins.emailComposer) {
-            window.plugins.emailComposer.showEmailComposerWithCallback(function(result) {
-                console.log("Response -> " + result);
-            },
-            "Foto", // Subject
-            "Segue a foto",                      // Body
-            [email],    // To
-            null,                    // CC
-            null,                    // BCC
-            false,                   // isHTML
-            [imageURI.replace('file://', '')],                    // Attachments
-            null);                   // Attachment Data
-        }
-    }
+		if(window.plugins && window.plugins.emailComposer) {
+			window.plugins.emailComposer.showEmailComposerWithCallback(function(result) {
+				console.log("Response -> " + result);
+			},
+			"Foto", // Subject
+			"Segue a foto",                      // Body
+			[email],    // To
+			null,                    // CC
+			null,                    // BCC
+			false,                   // isHTML
+			[imageURI.replace('file://', '')],                    // Attachments
+			null);                   // Attachment Data
+		}
+	}
 
-    var tirarFoto = function(email) {
-     navigator.camera.getPicture(function(imageURI) {
-         console.log(imageURI);
-         enviarEmail(email, imageURI);
-     }, function(err) {
+	var tirarFoto = function(email) {
+	 navigator.camera.getPicture(function(imageURI) {
+		 console.log(imageURI);
+		 enviarEmail(email, imageURI);
+	 }, function(err) {
 
-     }, {});
+	 }, {});
  }
 
  return {
-    tirarFoto: tirarFoto
+	tirarFoto: tirarFoto
 };
 }]);
