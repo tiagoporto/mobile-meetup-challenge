920925f295557a0fc9a83f12530d745585247be4
Tiago Porto <tiago@tiagoporto.com>
2015-04-18 13:51:55 -0300

    blah

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,14 +1,33 @@
 angular.module('devmeetup.github', [])
 
-.controller('GithubCtrl', function($scope, $http) {
+.controller('GithubCtrl', ['$scope','$http','github.service', 'github.foto','$ionicModal', function($scope, $http, githubService, githubFoto, $ionicModal) {
 
-	$scope.validar = function(){
-		$http.get("https://api.github.com/users/tiagoporto").then(function(result){
-			console.log(result);
-		})
+	$scope.model = {username: 'romulobr'}
+	$ionicModal.fromTemplateUrl('templates/picture-modal.html', {
+		scope: $scope,
+		animation: 'slide-in-up'
+	}).then(function(modal) {
+		$scope.modal = modal;
+	});
+
+	$scope.openModal = function() {
+		$scope.modal.show();
+	};
+	$scope.closeModal = function() {
+		$scope.modal.hide();
+	};
+
+  $scope.validar = function(){
+	githubService.getUser($scope.model.username).then(function (data) {
+		$scope.userData = data;
+		console.log(data);
+		$scope.openModal();
+	})}
+
+	$scope.tirarFoto = function() {
+		githubFoto.
 	}
-
-})
+}])
 
 .controller('FotoCtrl', function($scope) {
 
--- www/js/service-github.js
+++ www/js/service-github.js
@@ -3,11 +3,22 @@ angular.module('devmeetup.github')
 .factory('github.service', ['$http', function($http) {
 	var getUser = function(username){
 
-		$http.get("https://api.github.com/users/" + username).then(function(result){
-			console.log(result);
+		return $http.get("https://api.github.com/users/" + username).then(function(result){
+			return result.data;
 		})
 
 	}
 
+	return {getUser: getUser};
 
- }]);
+ }])
+
+.factory('foto.service', ['$http', function($http) {
+	var tirarFoto = function() {
+	  		navigator.camera.getPicture(function(imageURI) {
+	  	}, function(err) {
+
+		}, cameraOptions);
+  	}
+  	return {tirarFoto: tirarFoto};
+}]);
